981a46920c79d29cfea223f7df44b85a6c14ba8b
hogsmill <info@hogsmill.com>
2023-10-24 16:44:37 +0100

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 
-FORCE=false
-NEW=false
-while [ $1 ]
-do
-  echo $1
-  if [ "$1" == "-f" ]; then
-    FORCE=true
-  fi
-  shift
-done
-
-PORT=4103
-REPO="https://github.com/hogsmill/maykind.git"
-APP="maykind"
-PROPERTYCOLLECTION="maykindProperties"
-SITECOLLECTION="maykindSite"
-APPNAME="May Kind"
-
-echo "------------------------------------------------"
-echo "Installing $APP ($SITECOLLECTION, $PROPERTYCOLLECTION, $PORT)"
-echo "------------------------------------------------"
-
-DIR="/usr/apps/$APP"
-if [ ! -d $DIR ]; then
-  git clone $REPO $DIR
+if [ "$1" == "-f" ]; then
+  FORCE=true
 fi
 
-cd $DIR
-
-rm $DIR/package-lock.json
-rm -rf $DIR/node_modules
-
+PORT=4103
 PWD=`pwd`
 APP=`basename $PWD`
 git stash
@@ -41,32 +14,19 @@ if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
   exit 0
 fi
 
-npm install --legacy-peer-deps
+npm install
 npm run build
-if [ ! -d /var/www/html/$APP/ ]; then
-  mkdir /var/www/html/$APP
-fi
-if [ -d /var/www/html/$APP/css ]; then
-  rm /var/www/html/$APP/css/*
-else
-  mkdir /var/www/html/$APP/css
-fi
-if [ -d /var/www/html/$APP/js ]; then
-  rm /var/www/html/$APP/js/*
-else
-  mkdir /var/www/html/$APP/js
-fi
+rm /var/www/html/$APP/css/*
+rm /var/www/html/$APP/js/*
 cp -R dist/* /var/www/html/$APP
 if [ -f "src/server.js" ]; then
-  SERVER=`ps -ef | grep server.js | grep "/$APP/" | awk {'print $2'}`
+  SERVER=`ps -ef | grep server.js | grep $PORT | awk {'print $2'}`
   if [ "$SERVER" != "" ]; then
     kill -9 $SERVER
   fi
 fi
-rm -rf $DIR/node_modules/.cache
-rm -rf $DIR/dist
 
-ps -ef | grep php | grep outdated
-if [ $? -eq 1 ]; then
-  php /usr/apps/monitor/src/lib/outdated.php &
-fi
+rm -rf node_modules/.cache
+rm -rf dist
+
+exit 0
